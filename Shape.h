3bd9243edf0ce55866b10ca0c0cf391057fcc526
GeStanley <geoffrey.stanley@gmail.com>
2014-07-16 03:07:33 -0400

    first implementation of shape

--- Shape.h
+++ Shape.h
@@ -1,4 +1,30 @@
 #ifndef SHAPE_H_
 #define SHAPE_H_
 
+using namespace std;
+
+class Shape
+{
+  //**** constructors ****//
+  Shape( const string & );
+
+  //**** getters and setters ****//
+  const long getIdNumber() const;
+  void setIdNumber( const long & );
+  const string getName() const;
+  void setName( const string & );
+  const string getDescription() const;
+  void setDescription( const string & );
+
+  void scale( const int & );
+  const long computeArea() const;
+  const long computePerimeter() const;
+
+private:
+  long idNumber;
+  string name;
+  string description;
+
+};
+
 #endif
